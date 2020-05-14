acab50268e00cb013697225787f5e1449738dd67
Qamar Stationwala <YoPappy0@gmail.com>
2020-05-13 18:37:41 -0700

    created backend signing route

--- routes/bucketRoutes.js
+++ routes/bucketRoutes.js
@@ -1,5 +1,30 @@
-const router  = require('express').Router();
+const router = require('express').Router();
+const aws = require('aws-sdk');
 
+const { S3BUCKET, S3REGION } = process.env;
 
+aws.config.region = S3REGION;
+
+router.get('/sign-s3', (req, res) => {
+    const s3 = new aws.S3();
+    const { fileName, fileType } = req.query;
+
+    s3.getSignedUrl('putObject', {
+        Bucket: S3BUCKET,
+        Key: fileName,
+        Expires: 60,
+        ContentType: fileType,
+        ACL: 'public-read'
+    }, (err, data) => {
+        if (err) {
+            console.log(err);
+            res.status(500).json(err)
+        }
+        res.json({
+            signedRequest: data,
+            url: `https://${S3BUCKET}.s3.amazonaws.com/${fileName}`
+        });
+    });
+});
 
 module.exports = router;
